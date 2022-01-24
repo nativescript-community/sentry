271efb110abf9a7f0be729a60d8031b30c9b50c0
Martin Guillon <martin.guillon@akylas.fr>
2022-01-24 21:10:59 +0100

    chore: lint

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
+// We need to import it so we patch the hub with global functions
+// aka. this has side effects
+import '@sentry/tracing';
 import * as Integrations from './integrations';
 import { NSSentry } from './nssentry';
 // logger.disable(); // this crashes for {N} for now
 export {
     addBreadcrumb, addGlobalEventProcessor,
-
-
     captureEvent, captureException,
-
     captureMessage,
     configureScope,
-
     getCurrentHub, getHubFromCarrier,
-
     Hub,
     Scope,
     setContext,
@@ -22,11 +20,6 @@ export {
     setUser,
     withScope
 } from '@sentry/core';
-
-// We need to import it so we patch the hub with global functions
-// aka. this has side effects
-import '@sentry/tracing';
-
 export { Breadcrumb, Event, Exception, Request, Response, SdkInfo, Severity, StackFrame, Stacktrace, Status, Thread, User } from '@sentry/types';
 export { NativescriptBackend, NativescriptOptions } from './backend';
 export { NativescriptClient as NativescriptClient } from './client';
@@ -34,13 +27,12 @@ export { init, nativeCrash, setDist, setRelease } from './sdk';
 export {
     NSTracing
 } from './tracing';
-
-
 export { SDK_NAME, SDK_VERSION } from './version';
 export { Integrations };
-const captureUserFeedback = NSSentry.captureUserFeedback;
 export {
     captureUserFeedback
 };
+
+const captureUserFeedback = NSSentry.captureUserFeedback;
 
 
