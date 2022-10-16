91ae3f11823430534cfb9376dfd4f8e643eb5aee
Martin Guillon <dev@akylas.fr>
2022-10-16 18:59:33 +0200

    fix: version get fix

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 export const SDK_PACKAGE_NAME = 'npm:@nativescript-community/sentry';
 export const SDK_NAME = 'sentry.nativescript';
-export const SDK_VERSION = require('package.json').version;
+export const SDK_VERSION = require('./package.json').version;
